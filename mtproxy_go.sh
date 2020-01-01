442989acf70656781efe6afafe38d73b6421f0b4
王鑫 <wx071611@163.com>
2020-01-01 13:26:28 +0800

    init files

--- mtproxy_go.sh
+++ mtproxy_go.sh
@@ -113,7 +113,7 @@ Download(){
 }
 Service(){
 	if [[ ${release} = "centos" ]]; then
-		if ! wget --no-check-certificate "https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/service/mtproxy_go_centos" -O /etc/init.d/mtproxy-go; then
+		if ! wget --no-check-certificate "https://raw.githubusercontent.com/wx0716/Proxy/master/mtproxy_go_centos.sh" -O /etc/init.d/mtproxy-go; then
 			echo -e "${Error} MTProxy服务 管理脚本下载失败 !"
 			rm -rf "${file}"
 			exit 1
